346fc6558880562db98eab7d9d16f651ff9f3e1b
LiamBrem <liam.e.brem@gmail.com>
2023-08-28 13:25:21 -0400

    students in DB created from txt file of students

--- init_db.py
+++ init_db.py
@@ -1,16 +1,24 @@
 import sqlite3
 
-connection = sqlite3.connect('database.db')
+
+connection = sqlite3.connect("database.db")
 
 
-with open('schema.sql') as f:
+with open("schema.sql") as f:
     connection.executescript(f.read())
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO students VALUES ('Liam Brem')")
+File_object = open(r"allStudents.txt", "r")  # r stands for read only
 
-cur.execute("INSERT INTO students VALUES ('Dawson Krug')")
+listOfNames = File_object.readlines()
+
+# Loop through the strings and remove the last two characters & inserts each name from text file into db
+for i in range(len(listOfNames)):
+    listOfNames[i] = listOfNames[i][:-1]
+    cur.execute("INSERT INTO students VALUES ('" + listOfNames[i] + "')")
+File_object.close()
+
 
 connection.commit()
 connection.close()
