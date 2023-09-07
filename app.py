72bbf88584bfeac1f12721479fa3f1b2caf167a5
LiamBrem <liam.e.brem@gmail.com>
2023-09-07 12:52:26 -0400

    can't do 2 in under 5 mins- commented out for dev

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify, request, redirect, session, url_for
 import sqlite3
-from datetime import datetime
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 app.secret_key = "asdf"  # Needed to encrypt session data
@@ -119,9 +119,38 @@ def send_name():
     student = cursor.fetchone()
 
     if student:  # If student exists in the DB
+
+        # Commented out for development purposes 
+        '''
+        # Check if it has been 5 minutes since the latest addition to the database
+        cursor.execute(
+            """
+            SELECT MAX(timeLeft) FROM student_history
+            WHERE student_id = ?""",
+            (student[0],)
+        )
+        latest_timestamp = cursor.fetchone()[0]
+
+        if latest_timestamp:
+            latest_timestamp = datetime.strptime(latest_timestamp, "%Y-%m-%d %H:%M:%S.%f")
+            current_time = datetime.now()
+            if current_time - latest_timestamp > timedelta(minutes=5):
+                # If it has been more than 5 minutes, store student data in session
+                session["student"] = student
+                return "success"
+            else:
+                print("not 5 minutes")
+                return "It has not been 5 minutes since the latest addition."
+        else:
+            return "No history found for this student."
+        '''
+       
+
+        ### REMOVE IN PROD 
         # stores student data in session
         session["student"] = student
         return "success"
+        
     else:
         print("student not found")
         return "Student not found."
